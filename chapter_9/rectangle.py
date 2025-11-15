a55682f49b69a1b827b0fac22ab9422b990b58a1
Temesgen Geta <nigusgetamamo@gmail.com>
2025-11-15 05:46:07 +0300

    remove unwanted comments

--- chapter_9/rectangle.py
+++ chapter_9/rectangle.py
@@ -1,12 +1,5 @@
 from tkinter import *
 
-# wid = input("Enter the width of rectangle: ")
-# hei = input("Enter the height of rectangle: ")
-# bgcolor = input("Enter color of rectangle you went see: ")
-
-# x_position = int(input("Enter x-position: "))
-# y_position = int(input("Enter y-position: "))
-
 window = Tk()
 window.title("rectangle with users input")
 shape = Canvas(window,width = 200, height = 10, bg = "yellow").place(x = 300, y = 300)
